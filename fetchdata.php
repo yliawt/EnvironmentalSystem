4ee59c5df41a8e4b637b92a02115908d3df75800
Liaw Yee <86512609+yliawt@users.noreply.github.com>
2024-06-13 17:23:25 +0800

    Update fetchdata.php

--- fetchdata.php
+++ fetchdata.php
@@ -10,14 +10,12 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, temperature, humidity, Date, Time FROM nodemcu_table ORDER BY id DESC LIMIT 10";
+$sql = "SELECT id, temperature, humidity, Date, Time FROM nodemcu_table ORDER BY id DESC LIMIT 1";
 $result = $conn->query($sql);
 
 $data = array();
 if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        $data[] = $row;
-    }
+    $data = $result->fetch_assoc();
 }
 
 echo json_encode($data);
