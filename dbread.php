5f6d8a2d82688e2c358f036a5516f836ab9230d7
Liaw Yee <86512609+yliawt@users.noreply.github.com>
2024-06-13 17:22:50 +0800

    Update dbread.php

--- dbread.php
+++ dbread.php
@@ -1,46 +1,30 @@
-
 <?php
-
-
-
-    $host = "localhost";		         // host = localhost because database hosted on the same server where PHP files are hosted
-    $dbname = "id22312400_environment";              // Database name
-    $username = "id22312400_yliawt02";		// Database username
-    $password = "ThankYou979!";	        // Database password
-
+$host = "localhost";         // host = localhost because database hosted on the same server where PHP files are hosted
+$dbname = "id22312400_environment"; // Database name
+$username = "id22312400_yliawt02";  // Database username
+$password = "ThankYou979!";         // Database password
 
 // Establish connection to MySQL database
 $conn = new mysqli($host, $username, $password, $dbname);
 
-
 // Check if connection established successfully
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected to mysql database. <br>";
 }
 
-else { echo "Connected to mysql database. <br>"; }
-
-
 // Select values from MySQL database table
-
-$sql = "SELECT id, temperature, humidity, date, time FROM nodemcu_table";  // Update your tablename here
-
+$sql = "SELECT id, temperature, humidity, date, time FROM nodemcu_table"; // Update your tablename here
 $result = $conn->query($sql);
 
 echo "<center>";
 
-
-
 if ($result->num_rows > 0) {
-
-
     // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "<strong> Id:</strong> " . $row["id"]. " &nbsp <strong>Temperature:</strong> " . $row["temperature"]. " &nbsp <strong>Humidity:</strong> " . $row["humidity"]. " &nbsp <strong>Date:</strong> " . $row["date"]." &nbsp <strong>Time:</strong>" .$row["time"]. "<p>";
-    
-
-
-}
+    while ($row = $result->fetch_assoc()) {
+        echo "<strong> Id:</strong> " . $row["id"] . " &nbsp <strong>Temperature:</strong> " . $row["temperature"] . " &nbsp <strong>Humidity:</strong> " . $row["humidity"] . " &nbsp <strong>Date:</strong> " . $row["date"] . " &nbsp <strong>Time:</strong> " . $row["time"] . "<p>";
+    }
 } else {
     echo "0 results";
 }
@@ -48,7 +32,4 @@ if ($result->num_rows > 0) {
 echo "</center>";
 
 $conn->close();
-
-
-
 ?>
