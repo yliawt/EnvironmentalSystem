c9173bfe2a2b63a1c1d2d7db1c0e920422f70dbe
Liaw Yee <86512609+yliawt@users.noreply.github.com>
2024-06-30 01:17:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -32,29 +32,56 @@ setInterval(fetchData, 5000);
 });
 
 function fetchData() {
-$.ajax({
-url: 'fetch_data.php',
-type: 'get',
-dataType: 'json',
-success: function(data) {
-if (data.error) {
-console.error("Server Error:", data.error);
-} else {
-updateChart(tempChart, data.temperature);
-updateChart(humidityChart, data.humidity);
-updateChart(gasChart, data.gasLevel);
-}
-},
-error: function(xhr, status, error) {
-console.error("AJAX Error:", status, error);
-}
-});
+    $.ajax({
+        url: 'fetch_data.php',
+        type: 'get',
+        dataType: 'json',
+        success: function(data) {
+            if (data.error) {
+                console.error("Server Error:", data.error);
+            } else {
+                const lastData = data.temperature[data.temperature.length - 1];
+                $("#latestDate").text(lastData.timestamp.split(' ')[0]);
+                $("#latestTemp").text(lastData.value);
+                $("#latestHumidity").text(data.humidity[data.humidity.length - 1].value);
+                $("#latestGas").text(data.gasLevel[data.gasLevel.length - 1].value);
+
+                updateChart(tempChart, data.temperature);
+                updateChart(humidityChart, data.humidity);
+                updateChart(gasChart, data.gasLevel);
+
+                updateTable(data);
+            }
+        },
+        error: function(xhr, status, error) {
+            console.error("AJAX Error:", status, error);
+        }
+    });
 }
 
+function updateTable(data) {
+    const table = $("#recentDataTable");
+    table.empty();
+    data.temperature.forEach((temp, index) => {
+        const row = `<tr>
+            <td>${temp.timestamp.split(' ')[0]}</td>
+            <td>${temp.timestamp.split(' ')[1]}</td>
+            <td>${temp.value}°C</td>
+            <td>${data.humidity[index].value}%</td>
+            <td>${data.gasLevel[index].value}ppm</td>
+        </tr>`;
+        table.append(row);
+    });
+}
+
+
 function updateChart(chart, dataPoints) {
-chart.data.labels = dataPoints.map(d => d.timestamp);
-chart.data.datasets.forEach((dataset) => {
-dataset.data = dataPoints.map(d => d.value);
-});
-chart.update();
+    chart.data.labels = dataPoints.map(d => {
+        // Split the timestamp into date and time, and return only the time part
+        return d.timestamp.split(' ')[1];
+    });
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data = dataPoints.map(d => d.value);
+    });
+    chart.update();
 }
